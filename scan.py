3e964ea23fc0ef787be201187728fcb59a6d57a1
Yusuke Watanabe <isgsktyktt@gmail.com>
2024-08-24 19:28:29 +0900

    ignore, scan record

--- scan.py
+++ scan.py
@@ -19,17 +19,19 @@ def main():
     assert(scan_bin.exists())
     score_sum = 0
     score_norm = 0
-    for i in range(100):
-        cmd = str(scan_bin)
-        #cmd = "python sample/sample_submission.py"
-        cmd += "< tools/in/{0:04d}.txt > tools/out/{0:04d}.txt".format(i, i)
-        t0 = time.time()
-        ret = getoutput(cmd)
-        t1 = time.time()
-        score = int(ret)
-        score_sum += score
-        score_norm += 1
-        print(i, score, "{}ms".format(int(1000 * (t1 - t0))), "ave", score_sum / score_norm)
+    with open("score1.csv", "w") as f:
+        for i in range(100):
+            cmd = str(scan_bin)
+            #cmd = "python sample/sample_submission.py"
+            cmd += "< tools/in/{0:04d}.txt > tools/out/{0:04d}.txt".format(i, i)
+            t0 = time.time()
+            ret = getoutput(cmd)
+            t1 = time.time()
+            score = int(ret)
+            score_sum += score
+            score_norm += 1
+            print(i, score, "{}ms".format(int(1000 * (t1 - t0))), "ave", score_sum / score_norm)
+            f.write("{}\n".format(score))
     print("ave", score_sum / score_norm)
 
 if __name__ == "__main__":
